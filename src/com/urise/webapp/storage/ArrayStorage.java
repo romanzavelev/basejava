b285e5d847fe1264f486a24e1b72a588c73244dd
Zavelev Roman <rzav@mail.ru>
2021-05-20 21:31:53 +0300

    ArrayStorage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -59,7 +59,7 @@ public class ArrayStorage {
     }
 
     /**
-     * @return array, contains only Resumes in storage (without null) _
+     * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
