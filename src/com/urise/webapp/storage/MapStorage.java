2047dfe71c4c3d8829cda31278f9aaed5d176a4b
Роман <rzav@mail.ru>
2023-04-07 17:11:06 +0300

    HW5

---
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -0,0 +1,55 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.*;
+
+public class MapStorage extends AbstractStorage{
+
+    protected TreeMap<String,Resume> storage = new TreeMap<>();
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+        storage.put((String) searchKey, r);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+        storage.put((String) searchKey, r);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove((String) searchKey);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get((String) searchKey);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return storage.containsKey((String) searchKey);
+    }
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return uuid;
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
